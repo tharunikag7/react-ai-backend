fffa92cc35b60e15018d518acf88d169212f0295
tharunikag7 <tharunikag7@gmail.com>
2026-03-23 12:30:18 +0530

    fix port for render

--- server.js
+++ server.js
@@ -75,6 +75,8 @@ res.json({
 });
 
 // ✅ Start server
-app.listen(5000, () => {
-  console.log("Server running on http://localhost:5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
